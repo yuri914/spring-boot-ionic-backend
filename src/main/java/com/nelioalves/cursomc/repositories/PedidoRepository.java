56fbf82543f27733ce5a044519db3290c149f81f
Yuri Sousa <yuri.sousa@geq.com.br>
2018-11-06 11:30:19 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/nelioalves/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,24 @@
 package com.nelioalves.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.Pedido;
+import com.nelioalves.cursomc.dto.CategoriaDTO;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 
+    @Transactional(readOnly = true)
+    Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
+    
 }
--- src/main/java/com/nelioalves/cursomc/services/PedidoService.java
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.ItemPedido;
 import com.nelioalves.cursomc.domain.PagamentoComBoleto;
 import com.nelioalves.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
 import com.nelioalves.cursomc.repositories.ItemPedidoRepository;
 import com.nelioalves.cursomc.repositories.PagamentoRepository;
 import com.nelioalves.cursomc.repositories.PedidoRepository;
+import com.nelioalves.cursomc.security.UserSS;
+import com.nelioalves.cursomc.services.exceptions.AuthorizationException;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 
@@ -73,6 +79,17 @@ public class PedidoService {
 	itemPedidoRepository.saveAll(obj.getItens());
 	emailService.sendOrderConfirmateHtmlEmail(obj);
 	return obj;
+    }
+    
+    public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+	UserSS user = UserService.authenticated();
+	
+	if(user == null) {
+	    throw new AuthorizationException("Acesso negado");
+	}
+	PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+	Cliente cliente = clienteService.find(user.getId());
+	return pedidoRepository.findByCliente(cliente, pageRequest);
     }
     
 }

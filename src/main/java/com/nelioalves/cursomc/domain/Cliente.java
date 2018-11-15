30f5879b11bcf2b122a0e40df6a9451a9ebaf031
Yuri Sousa <yuricavalcante1@gmail.com>
2018-11-15 18:18:10 -0300

    Salvando a URL da imagem em Cliente

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -55,6 +55,8 @@ public class Cliente implements Serializable {
     @OneToMany(mappedBy = "cliente")
     private List<Pedido> pedidos = new ArrayList<>();
     
+    private String imageUrl;
+    
     public Cliente() {
 	addPerfil(Perfil.CLIENTE);
     }
@@ -148,6 +150,14 @@ public class Cliente implements Serializable {
 
     public void setPedidos(List<Pedido> pedidos) {
         this.pedidos = pedidos;
+    }
+    
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 
     @Override
--- src/main/java/com/nelioalves/cursomc/services/ClienteService.java
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -116,6 +116,17 @@ public class ClienteService {
     }
     
     public URI uploadProfilePricture(MultipartFile multipartFile) {
-	return s3Service.uploadFile(multipartFile);
+	UserSS user = UserService.authenticated();
+	if(user == null) {
+	    throw new AuthorizationException("Acesso negado.");
+	}
+	
+	URI uri = s3Service.uploadFile(multipartFile);
+	
+	Cliente cliente = clienteRepository.findById(user.getId()).get();
+	cliente.setImageUrl(uri.toString());
+	clienteRepository.save(cliente);
+	
+	return uri;
     }
 }
